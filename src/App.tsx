a650821ab114b0451d366f161ecd2d84c32a1270
naftalimurgor <murgornaftali@gmail.com>
2021-12-27 19:05:39 +0300

    Add `src` with TS migration and `tsconfig.json` file

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import Fork from './components/fork';
+import Gallary from './containers/gallary';
+import SelectedImage from './containers/selectedImage';
+
+class App extends Component {
+  render() {
+    return (
+      // @ts-ignore
+      <Provider store={store}>
+        <div>
+          <Fork />
+          <Gallary />
+          <SelectedImage />
+        </div>
+      </Provider>
+    );
+  }
+}
+
+export default App;
